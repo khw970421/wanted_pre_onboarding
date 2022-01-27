e0aef0892dde35175f9d8c76c09d4470e99c545b
khw970421 <khw970421@kakao.com>
2022-01-27 17:41:17 +0900

    🐛 fix : animation을 위해 코드 수정

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -1,45 +1,61 @@
 import styled from "@emotion/styled";
 import { useState } from "react";
-const Button = styled.button`
-  margin: 0px;
-  border: 0px;
-  padding: 1% 6px;
+
+const ToggleContainer = styled.label`
+  display: inline-block;
+  cursor: pointer;
+  user-select: none;
+`;
+
+const ToggleSwitch = styled.div`
   width: ${(props) => props.width}px;
   height: ${(props) => props.height}px;
+  padding: 4px;
+  box-sizing: border-box;
+  background-color: #ccc;
   border-radius: ${(props) => props.height}px;
-  background-color: ${(props) =>
-    props.isClick ? "purple" : "rgb(220, 220, 220, 0.5)"};
-  position: relative;
+
+  &:after {
+    content: "";
+    position: relative;
+    left: 0;
+    display: block;
+    width: ${(props) => props.height - 8}px;
+    height: ${(props) => props.height - 8}px;
+    border-radius: 50%;
+    background-color: white;
+    transition: left 0.5s ease-out;
+  }
 `;
 
-const ButtonToggle = styled.div`
-  background-color: white;
-  position: absolute;
-  border-radius: 50%;
-  top: 50%;
-  transform: translate(0, -50%);
-  left: ${(props) => (props.isClick ? "" : "8%")};
-  right: ${(props) => (props.isClick ? "8%" : "")};
-  width: ${(props) => props.height * 0.8}px;
-  height: ${(props) => props.height * 0.8}px;
-  border-radius: ${(props) => props.height * 0.8}px;
-  transition: left 1s ease-out;
+const ToggleInput = styled.input`
+  display: none;
+
+  &:checked + div {
+    background: purple;
+  }
+
+  &:checked + div:after {
+    left: calc(100% - ${(props) => props.height - 8}px);
+  }
 `;
 
-const Toggle = ({ width = 300, height = 100 }) => {
-  const [isClick, setIsClick] = useState(false);
-  const clickToggle = () => {
-    setIsClick(!isClick);
+const Toggle = ({ width = 50, height = 30, disabled }) => {
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => {
+    setChecked(!checked);
   };
   return (
-    <Button
-      width={width}
-      height={height}
-      onClick={clickToggle}
-      isClick={isClick}
-    >
-      <ButtonToggle width={width} height={height} isClick={isClick} />
-    </Button>
+    <ToggleContainer>
+      <ToggleInput
+        type="checkbox"
+        checked={checked}
+        disabled={disabled}
+        onChange={handleChange}
+        height={height}
+      />
+      <ToggleSwitch width={width} height={height} />
+    </ToggleContainer>
   );
 };
 
