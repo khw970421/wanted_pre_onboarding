0fc0298dcb1e1dd3caeca628a8152c1aeb8fe7f5
khw970421 <khw970421@kakao.com>
2022-01-30 17:44:40 +0900

    💄 styles : styled-components 아래로 옮기기

--- custom-component/src/component/Tab.js
+++ custom-component/src/component/Tab.js
@@ -2,29 +2,13 @@ import styled from "@emotion/styled";
 import { useState } from "react";
 const exampleTabArr = ["Tab1", "Tab2", "Tab3", "Tab4"];
 
-const MenuContainer = styled.div``;
-
-const MenuTable = styled.div`
-  display: flex;
-`;
-
-const MenuItem = styled.div`
-  width: calc(100vw / ${({ tabArr }) => tabArr.length});
-  background-color: ${({ selectedId, id }) =>
-    selectedId === id ? "purple" : "rgba(0,0,0,0.1)"};
-    color : ${({ selectedId, id }) =>
-    selectedId === id ? "white" : "rgba(0,0,0,0.6)"};
-`;
-
-const MenuResult = styled.div``;
-
-const Tab = ({ width = 40, height = 30, tabArr = exampleTabArr }) => {
+const Tab = ({ width = 40, height = 10, tabArr = exampleTabArr }) => {
   const [selectedTab, setSelectedTab] = useState(0);
   const clickMenuTab = ({ target }) => {
     setSelectedTab(Number(target.id));
   };
   return (
-    <MenuContainer width={width} height={height}>
+    <MenuContainer>
       <MenuTable>
         {tabArr &&
           tabArr.map((tab, id) => (
@@ -40,9 +24,40 @@ const Tab = ({ width = 40, height = 30, tabArr = exampleTabArr }) => {
             </MenuItem>
           ))}
       </MenuTable>
-      <MenuResult>Tab menu {tabArr[selectedTab]}</MenuResult>
+      <MenuResult width={width} height={height}>
+        Tab menu {tabArr[selectedTab]}
+      </MenuResult>
     </MenuContainer>
   );
 };
+
+const MenuContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MenuTable = styled.div`
+  display: flex;
+`;
+
+const MenuItem = styled.div`
+  width: calc(${({ width }) => width}vw / ${({ tabArr }) => tabArr.length});
+  height: ${({ height }) => height}vh;
+  background-color: ${({ selectedId, id }) =>
+    selectedId === id ? "purple" : "rgba(0,0,0,0.1)"};
+  color: ${({ selectedId, id }) =>
+    selectedId === id ? "white" : "rgba(0,0,0,0.6)"};
+  display: flex;
+  align-items: center;
+  padding: 1%;
+  cursor: pointer;
+`;
+
+const MenuResult = styled.div`
+  width: ${({ width }) => width}vw;
+  height: ${({ height }) => height}vh;
+`;
 
 export default Tab;
