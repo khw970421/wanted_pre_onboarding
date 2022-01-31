a8d17d0cac8aa2d02981a1b5ebea726909175b01
khw970421 <khw970421@kakao.com>
2022-01-31 18:20:33 +0900

    ✨ feat :  검색된 값을 클릭시 해당 값으로 input 값 변경 코드 추가

--- custom-component/src/component/AutoComplete.js
+++ custom-component/src/component/AutoComplete.js
@@ -17,24 +17,44 @@ const EraseBtn = styled.button``;
 const Input = styled.input``;
 
 const AutoComplete = () => {
-  const [searchIncludeArr, setSearchIncludeArr] = useState("");
+  const [searchIncludeArr, setSearchIncludeArr] = useState([]);
   const [isSearchValueOpen, setIsSearchValueOpen] = useState(false);
 
   const retSearchIncludeArr = (value) =>
     exampleArr.filter((saveValue) => saveValue.indexOf(value) >= 0);
 
   const changeInput = ({ target }) => {
-    if (target.value !== "") {
-      setSearchIncludeArr(retSearchIncludeArr(target.value));
+    const value = target.value;
+    if (value !== "") {
+      const retArr = retSearchIncludeArr(value);
+      setSearchIncludeArr(retArr);
       setIsSearchValueOpen(true);
     } else {
       setIsSearchValueOpen(false);
     }
   };
+
+  const clickSearchValue = ({ target }) => {
+    const $Input = document.querySelector(".inputSearch");
+    $Input.value = target.innerText;
+    setSearchIncludeArr([]);
+  };
   return (
     <InputContainer>
-      <Input type="search" onChange={changeInput} />
-      {isSearchValueOpen && searchIncludeArr.map((val) => <div>{val}</div>)}
+      <Input
+        type="search"
+        onChange={changeInput}
+        autocomplete="off"
+        className="inputSearch"
+      />
+      <ul className="item-ul">
+        {isSearchValueOpen &&
+          searchIncludeArr.map((val, idx) => (
+            <li onClick={clickSearchValue} id={idx}>
+              {val}
+            </li>
+          ))}
+      </ul>
     </InputContainer>
   );
 };
