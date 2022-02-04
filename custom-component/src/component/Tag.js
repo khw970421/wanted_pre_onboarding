a7df4d307da179ab2d9ccd21f129ec9e80ea7754
khw970421 <khw970421@kakao.com>
2022-02-04 16:14:21 +0900

    💄 styles : styled components 코드 아래 옮기기

--- custom-component/src/component/Tag.js
+++ custom-component/src/component/Tag.js
@@ -1,5 +1,46 @@
 import styled from "@emotion/styled";
 import { useState, useRef } from "react";
+
+const Tag = ({ width = 500, height = 50 }) => {
+  const [tagArr, setTagArr] = useState([]);
+  const [inputFocus, setInputFocus] = useState(false);
+  const nameInput = useRef();
+  const changeInputEvent = ({ key, target }) => {
+    if (key === "Enter" && target.value !== "") {
+      setTagArr([...tagArr, target.value]);
+      target.value = "";
+      nameInput.current.focus();
+    }
+  };
+
+  const eraseTagElement = ({ target }) => {
+    const { id } = target.parentNode;
+    setTagArr(tagArr.filter((_, idx) => Number(id) !== idx));
+  };
+
+  const focusInput = () => {
+    setInputFocus(!inputFocus);
+  };
+  return (
+    <TagContainer width={width} height={height} inputFocus={inputFocus}>
+      {tagArr.map((val, idx) => (
+        <TagElementContainer id={idx} key={idx}>
+          <ContentDiv> {val}</ContentDiv>
+          <TagEraseBtn onClick={eraseTagElement}>X</TagEraseBtn>
+        </TagElementContainer>
+      ))}
+
+      <TagInput
+        placeholder="Press enter to add tags"
+        onKeyUp={changeInputEvent}
+        ref={nameInput}
+        onFocus={focusInput}
+        onBlur={focusInput}
+      />
+    </TagContainer>
+  );
+};
+
 const TagContainer = styled.div`
   display: flex;
   align-items: center;
@@ -8,7 +49,8 @@ const TagContainer = styled.div`
   height: ${({ height }) => height}px;
   min-width: 300px;
   max-width: 500px;
-  border:${({inputFocus})=>inputFocus ? "1.5px solid black" : "1px solid gray"} ;
+  border: ${({ inputFocus }) =>
+    inputFocus ? "1.5px solid black" : "1px solid gray"};
   overflow: scroll;
   &::-webkit-scrollbar {
     display: none; /* Chrome, Safari, Opera*/
@@ -49,45 +91,5 @@ const ContentDiv = styled.div`
   white-space: no-wrap;
   block: inline-block;
 `;
-
-const Tag = ({ width = 500, height = 50 }) => {
-  const [tagArr, setTagArr] = useState([]);
-  const [inputFocus, setInputFocus] = useState(false);
-  const nameInput = useRef();
-  const changeInputEvent = ({ key, target }) => {
-    if (key === "Enter") {
-      setTagArr([...tagArr, target.value]);
-      target.value = "";
-      nameInput.current.focus();
-    }
-  };
-
-  const eraseTagElement = ({ target }) => {
-    const { id } = target.parentNode;
-    setTagArr(tagArr.filter((_, idx) => Number(id) !== idx));
-  };
-
-  const focusInput = () => {
-    setInputFocus(!inputFocus);
-  };
-  return (
-    <TagContainer width={width} height={height} inputFocus={inputFocus}>
-      {tagArr.map((val, idx) => (
-        <TagElementContainer id={idx} key={idx}>
-          <ContentDiv> {val}</ContentDiv>
-          <TagEraseBtn onClick={eraseTagElement}>X</TagEraseBtn>
-        </TagElementContainer>
-      ))}
-
-      <TagInput
-        placeholder="Press enter to add tags"
-        onKeyUp={changeInputEvent}
-        ref={nameInput}
-        onFocus={focusInput}
-        onBlur={focusInput}
-      />
-    </TagContainer>
-  );
-};
 
 export default Tag;
